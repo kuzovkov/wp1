a886d9809ee72e43f083ae20c6a1725ad9d87f8e
Your Name <you@example.com>
2016-05-01 13:46:25 +0300

    next

--- wp-content/plugins/dseller/views/products_page.php
+++ wp-content/plugins/dseller/views/products_page.php
@@ -44,24 +44,36 @@
     <div class="form-group"><label for="file">File:</label><input type="file" class="form-control" id=file name="file" value=""/></div>
     <button name="dseller_product_add_btn" type="submit" class="btn btn-default">Добавить</button>
 </form>
+<hr/>
 
 <h2>Список товаров</h2>
 
 <?php if ($products): ?>
+    <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>Название</th>
+            <th>Цена</th>
+            <th>URL</th>
+            <th></th>
+            <th></th>
+        </tr>
 <?php foreach($products as $item):?>
-    <p>ID: <?php echo $item->id?></p>
     <form class="opt-form" name="dseller_form" method="post" action="<?php echo $_SERVER['HTTP_SELF']?>?page=dseller-products-opt&amp;update=true">
         <?php if(function_exists('wp_nonce_field')) wp_nonce_field( 'dseller_form' ); ?>
-        <div class="form-group"><label for="name">Название:</label><input id="name" class="form-control" name="name" value="<?php echo $item->name;?>"/></div>
-        <div class="form-group"><label for="price">Цена:</label><input id="price" class="form-control" name="price" value="<?php echo $item->cost;?>"/></div>
-        <div class="form-group"><label for="url">URL:</label><input class="form-control" id="url" name="url" value="<?php echo $item->url;?>"/></div>
-        <input type="hidden" name="id" value="<?php echo $item->id;?>"/>
-        <button name="dseller_product_update_btn" type="submit" class="btn btn-default">Сохранить</button>
-        <button name="dseller_product_del_btn" type="submit" class="btn btn-default">Удалить</button>
+        <tr>
+            <td><?php echo $item->id?></td>
+            <td><input id="name" class="form-control" name="name" value="<?php echo $item->name;?>"/></td>
+            <td><input id="price" class="form-control" name="price" value="<?php echo $item->cost;?>"/></td>
+            <td><input class="form-control" id="url" name="url" value="<?php echo $item->url;?>"/></td>
+            <input type="hidden" name="id" value="<?php echo $item->id;?>"/>
+            <td><button name="dseller_product_update_btn" type="submit" class="btn btn-default">Сохранить</button></td>
+            <td><button name="dseller_product_del_btn" type="submit" class="btn btn-default">Удалить</button></td>
+        </tr>
     </form>
-    <hr/>
 
 <?php endforeach;?>
+    </table> 
 <?php endif; ?>
 
 
